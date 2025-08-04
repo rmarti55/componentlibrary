9e598cce41e9216fea725b8e828bc1e3c5cea2be
rmarti55 <rmarti55@users.noreply.github.com>
2025-08-04 14:28:27 -0600

    Remove unused COMPONENT_MAP import

--- src/components/AIChatPreview.tsx
+++ src/components/AIChatPreview.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { DynamicComponentRenderer } from './DynamicComponentRenderer'
-import { ComponentDefinition, COMPONENT_MAP } from './TrustedComponentMap'
+import { ComponentDefinition } from './TrustedComponentMap'
 
 interface AIChatPreviewProps {
   code: string
